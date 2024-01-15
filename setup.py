e800a136b3c29df3ee4059968b6d0e37d0a4b23a
amirvhd <amir_vhd@yahoo.com>
2024-01-15 09:29:09 +0000

    change name of library

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""A setuptools-based script for installing the lotfollibrary package."""
+"""A setuptools-based script for installing the package."""
 
 from setuptools import find_packages, setup
 
@@ -6,7 +6,7 @@ with open('README.md') as f:
     long_description = f.read()
 
 setup(
-    name='lotfollibrary',
+    name='gplearner',
     packages=find_packages(),
     version='0.1.0',
     description='',
