f725c83328efa207d234541b8ad7e757664c3d89
chris-soelistyo <chrissoelistyo@gmail.com>
2025-01-14 14:48:44 +0000

    modified docstring

--- gplearner/Models/gp_model.py
+++ gplearner/Models/gp_model.py
@@ -1253,6 +1253,9 @@ class gpTransformerGlobal(gpTransformerBase):
 class gpTransformerGlobalLinear(gpTransformerGlobal):
     """Equivalent to gpTransformerGlobal, but with no cell token learner
     in the forward pass.
+    
+    The cell token is simply taken to be the gp token of the first
+    gp of interest.
     """
 
     def __init__(self, **kwargs):
