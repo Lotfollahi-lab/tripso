191efa1d4b2aadf2ede1c30928be862a5bc9ed81
Christopher Soelistyo <cs60@sanger.ac.uk>
2025-02-14 02:39:15 +0000

    made branch compatible with gpfinder

--- gplearner/Models/gp_model.py
+++ gplearner/Models/gp_model.py
@@ -1371,9 +1371,9 @@ class gpTransformerGlobal(gpTransformerBase):
 
         if return_gene_embeddings:
             return base_output
-        
+
         cell_output = self.base_output_to_cell_output(
-            base_output, 
+            base_output,
             masking_global=masking_global,
         )
 
