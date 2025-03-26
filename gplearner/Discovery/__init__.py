cf7eb65ca8606cec4c6b47e4be030683dcff67ca
chris-soelistyo <chrissoelistyo@gmail.com>
2025-03-26 17:29:12 +0000

    added clustering code

--- gplearner/Discovery/__init__.py
+++ gplearner/Discovery/__init__.py
@@ -1,14 +1,9 @@
-from .downstream import (
-    calculate_cell_token_attribution_scores,
-    calculate_gp_attribution_scores,
-    gpEval,
-    visualize_with_gene_exp,
+from .clustering import (
+    cluster,
+    rerank_genes,
 )
 
 __all__ = [
-    'gpEval',
-    'calculate_cell_token_attribution_scores',
-    'calculate_gp_attribution_scores',
-    'visualize_with_gene_exp',
+    'cluster',
+    'rerank_genes',
 ]
-
