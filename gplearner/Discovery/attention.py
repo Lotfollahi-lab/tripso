b192852d8f073c278a55080be339000b7a31671a
chris-soelistyo <chrissoelistyo@gmail.com>
2025-08-13 14:10:24 +0100

    updated function for notebook-friendliness

--- gplearner/Discovery/attention.py
+++ gplearner/Discovery/attention.py
@@ -4,30 +4,32 @@ import os
 
 import numpy as np
 import pandas as pd
-import scanpy as sc
+
+from anndata import AnnData
 
 def rank_genes_by_attn_diff(
-    attn_adata_fn: Union[str, os.PathLike],
+    attn_adata: AnnData,
     target_label: str,
     target_class: str,
-    output_fn: Union[str, os.PathLike],
     subset_obs_column: Optional[str] = None,
     subset_obs_values: Union[str, List[str], None] = None,
     subset_adata_fn: Union[str, os.PathLike, None] = None,
-):
-    attn_adata = sc.read_h5ad(attn_adata_fn)
+    save_subset_adata: bool = False,
+) -> pd.DataFrame:
 
     # subset the anndata
     if subset_obs_column is not None and subset_obs_values is not None:
-        
-        if subset_adata_fn is None:
-            subset_adata_fn = os.path.splitext(attn_adata_fn)[0] + f'_subset_{subset_obs_column}.h5ad'
-            
+
         if isinstance(subset_obs_values, str):
             subset_obs_values = [subset_obs_values]
         
         attn_adata = attn_adata[attn_adata.obs[subset_obs_column].isin(subset_obs_values)]
-        attn_adata.write_h5ad(subset_adata_fn)
+        
+        if save_subset_adata:
+            if subset_adata_fn is None:
+                raise ValueError('subset_adata_fn must be specified to save the subset adata.')
+            
+            attn_adata.write_h5ad(subset_adata_fn)
         
     # get attention diff
     attn_target_class = attn_adata[attn_adata.obs[target_label] == target_class]
@@ -43,4 +45,5 @@ def rank_genes_by_attn_diff(
     })
     attn_df = attn_df.sort_values(by='scores_diff', ascending=False)
     attn_df.rename(columns={'names': 'gene'}, inplace=True)
-    attn_df.to_csv(output_fn, index=False)
+    
+    return attn_df
